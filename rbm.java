834fc0cf01465f0b80221ce1d8718237c15af579
sunkairan <ksun@ufl.edu>
2013-10-13 17:06:01 -0400

    Update rbm.java

--- rbm.java
+++ rbm.java
@@ -9,39 +9,58 @@
 // batchdata -- the data that is divided into batches (numcases numdims numbatches)
 // restart   -- set to 1 if learning starts from beginning 
 
-package rbm;
+//import Jama.Matrix;
 import java.util.Random;
 import java.lang.Math;
 
 public class rbm {
-	private double epsilonw = 0.1; 
-	private double epsilonvb = 0.1;
-	private double espilonhb = 0.1;
-	private double weightcost = 0.0002;
-	private double initialmomentum = 0.5;
-	private double finalmomentum = 0.9;
+	private double epsilonw;
+	private double epsilonvb;
+	private double espilonhb;
+	private double weightcost;
+	private double initialmomentum;
+	private double finalmomentum;
 	private int numhid;
+	private int numdims;
+	private int numbatches;
+	private int maxepoch;
 	
-	public int numdims;
-	public int numbatches;
-	public int epoch;
-	public int maxepoch;
-	
-	private Matrix hidbiases = new Matrix(1,numhid);
-	private Matrix visbiases = new Matrix(1,numdims);
-	private Matrix poshidprobs = new Matrix(1,numhid);
-	private Matrix neghidprobs = new Matrix(1,numhid);
-	private Matrix posprods = new Matrix(numdims,numhid);
-	private Matrix negprods = new Matrix(numdims,numhid);
-	private Matrix vishidinc = new Matrix(numdims,numhid);
-	private Matrix hdibiasinc = new Matrix(1,numhid);
-	private Matrix visbiasinc = new Matrix(1,numhid);
-	private Matrix data = new Matrix(1,numdims);
-	private Matrix vishid = new Matrix(numdims,numhid);
+	private Matrix hidbiases;
+	private Matrix visbiases;
+	private Matrix poshidprobs;
+	private Matrix neghidprobs;
+	private Matrix posprods;
+	private Matrix negprods;
+	private Matrix vishidinc;
+	private Matrix hdibiasinc;
+	private Matrix visbiasinc;
+	private Matrix data;
+	private Matrix vishid;
 	
     //generate the initial random weights of each path	
     
-	private void  initialize(Matrix inputData){
+	private void  initialize(Matrix data, int numdims, int numhid){
+		epsilonw = 0.1; 
+		epsilonvb = 0.1;
+		espilonhb = 0.1;
+		weightcost = 0.0002;
+		initialmomentum = 0.5;
+		finalmomentum = 0.9;
+		
+		this.numhid = numhid;
+		this.numdims = numdims;
+		
+		this.data = data.copy();
+		hidbiases = new Matrix(1,numhid);
+		visbiases = new Matrix(1,numdims);
+		poshidprobs = new Matrix(1,numhid);
+		neghidprobs = new Matrix(1,numhid);
+		posprods = new Matrix(numdims,numhid);
+		negprods = new Matrix(numdims,numhid);
+		vishidinc = new Matrix(numdims,numhid);
+		hdibiasinc = new Matrix(1,numhid);
+		visbiasinc = new Matrix(1,numhid);
+		
 		double[][] temp1 = new double[numdims][numhid];
 		Random randomgenerator = new Random();
 		for(int i = 0; i < numdims; i++){
@@ -49,39 +68,36 @@ public class rbm {
 				temp1[i][j] = 0.1 * randomgenerator.nextGaussian();
 				}
 			}
-		vishid = new Matrix(temp1);
-		data = inputData;
+		vishid = new Matrix(temp1); //random Matrix(numdims,numhid);
 	}
 	//Start calculate the positive phase
 	//calculate the cured value of h0
 	
 	private Matrix getposphase(){
-		Matrix product_tmp1 = data.times(vishid);
+		poshidprobs = data.times(vishid);
 		//(1 * numdims) * (numdims * numhid)
-		product_tmp1.plusEquals(hidbiases);
+		poshidprobs.plusEquals(hidbiases);
 		//data*vishid + hidbiases
-		double [] product_tmp2 = product_tmp1.getRowPackedCopy();
+		double [] [] product_tmp2 = poshidprobs.getArray();
 		int i2 = 0;
-		while( i2 < product_tmp2.length){
-			double tmp = product_tmp2[i2];
-			product_tmp2[i2] = 1/(1 - Math.exp(tmp));
+		while( i2 < numhid){
+			product_tmp2[0][i2] = 1/(1 + Math.exp(-product_tmp2[0][i2]));
 			i2++;
 		}
-		poshidprobs = new Matrix(product_tmp2, numhid);
-		Matrix data_t = data.transpose();
-		posprods = data_t.times(poshidprobs);
+		posprods = data.transpose().times(poshidprobs);
 		
 	//end of the positive phase calculation, find the binary presentation of h0
 		int i3 =0;
-		double [] tmp1 = poshidprobs.getRowPackedCopy();
+		double [] [] tmp1 = poshidprobs.getArray();
+		double [] [] tmp2 = new double [1][numhid];
 		Random randomgenerator = new Random();
-		while (i3 < tmp1.length){
-			if (tmp1[i3] > randomgenerator.nextDouble())
-				tmp1[i3] = 1;
-			else tmp1[i3] = 0;
+		while (i3 < numhid){
+			if (tmp1[0][i3] > randomgenerator.nextDouble())
+				tmp2[0][i3] = 1;
+			else tmp2[0][i3] = 0;
 			i3++;
 			}
-		Matrix poshidstates = new Matrix(tmp1,tmp1.length);
+		Matrix poshidstates = new Matrix(tmp2);
 		return (poshidstates);
 		}
 	
@@ -89,31 +105,27 @@ public class rbm {
 	//calculate the curved value of v1,h1
 	private void getnegphase(Matrix poshidstates){
 		//find the vector of v1
-		Matrix product_tmp1 = poshidstates.times(vishid.transpose());
+		Matrix negdata = poshidstates.times(vishid.transpose());
 		//(1 * numhid) * (numhid * numdims) = (1 * numdims)
-		product_tmp1.plusEquals(visbiases);
+		negdata.plusEquals(visbiases);
 		//poshidstates*vishid' + visbiases
-		double [] tmp1 = product_tmp1.getRowPackedCopy();
+		double [] [] tmp1 = negdata.getArray();
 		int i1 = 0;
-		while( i1 < tmp1.length){
-			double tmp = tmp1[i1];
-			tmp1[i1] = 1/(1 - Math.exp(tmp));
+		while( i1 < numdims){
+			tmp1[0][i1] = 1/(1 + Math.exp(-tmp1[0][i1]));
 			i1++;
 		}
-		Matrix negdata = new Matrix(tmp1, tmp1.length); //tmp1.length = numdims
 		
 		//find the vector of h1
-		Matrix product_tmp2 = negdata.times(vishid);
+		neghidprobs = negdata.times(vishid);
 		//(1 * numdims) * (numdims * numhid) = (1 * numhid)
-		product_tmp2.plusEquals(hidbiases);
-		double [] tmp2 = product_tmp2.getRowPackedCopy();
+		neghidprobs.plusEquals(hidbiases);
+		double [] [] tmp2 = neghidprobs.getArray();
 		int i2 = 0;
-		while( i2 < tmp2.length){
-			double tmp = tmp2[i2];
-			tmp2[i2] = 1/(1 - Math.exp(tmp));
+		while( i2 < numhid){
+			tmp2[0][i2] = 1/(1 + Math.exp(-tmp2[0][i2]));
 			i2++;
 			}
-		neghidprobs = new Matrix(tmp2, tmp2.length);
 		negprods = negdata.transpose().times(neghidprobs);
 		//(numdims * 1) *(1 * numhid) = (numdims * numhid)
 		}
@@ -129,15 +141,18 @@ public class rbm {
 		vishidinc.timesEquals(momentum);
 	    Matrix temp1 = posprods.minus(negprods);
 	    Matrix temp2 = vishid.times(weightcost);
-	    temp1.minusEquals(temp2).timesEquals(epsilonw);
+	    temp1.minusEquals(temp2);
+	    temp1.timesEquals(epsilonw);
 	    vishidinc.plusEquals(temp1);
 	    //vishid = vishid + vishidinc;
 	    vishid.plusEquals(vishidinc);
 	    }
 	
-	public int run(int epochmax, Matrix inputData){
-		initialize(inputData);
-		for (int i = 1; i < epochmax; i++ ){
+	public int run(int epochmax, Matrix inputData, int numdims, int numhid){
+		initialize(inputData, numdims, numhid);
+		for (int i = 0; i < epochmax; i++ ){
+			System.out.print("Epoch: ");
+			System.out.println(i);
 			Matrix poshidstates = getposphase();
 			getnegphase(poshidstates);
 			update(i);
@@ -145,11 +160,4 @@ public class rbm {
 		return (0);
 		}
 	}
-		
 	
-
-	
-
-	
-		
-
